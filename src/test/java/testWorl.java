d4287bc95be907a39bf7301892ae2f063bb04c81
Dragon-Jack <1135045462@qq.com>
2019-11-26 13:23:20 +0800

    26号

--- src/test/java/testWorl.java
+++ src/test/java/testWorl.java
@@ -1,6 +1,10 @@
 import com.dfbz.config.SpringMybatis;
 import com.dfbz.dao.WorkMapper;
+import com.dfbz.entity.Detail;
+import com.dfbz.entity.Transfer;
+import com.dfbz.entity.Work;
 import com.dfbz.service.WorkSrevice;
+import com.github.pagehelper.PageInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +12,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.HashMap;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = SpringMybatis.class)
@@ -21,9 +26,9 @@ public class testWorl {
 
     @Test
     public void test(){
+        Transfer transport = srevice.transport(1);
+        System.out.println("***"+transport);
 
-
-        System.out.println( mapper.order(2)+"************");
     }
 
 }
